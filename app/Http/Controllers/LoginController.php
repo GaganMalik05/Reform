75e6754711d811288d8bb1951952bd4c359d47c0
Jwngdao <zwngdao2@gmail.com>
2025-06-12 14:01:38 +0530

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +10,27 @@ class LoginController extends Controller
     public function login()
     {
         return view("login");
+    }
+
+    public function loginsubmit(Request $request)
+    {
+        $credentials=$request->only('user_name','password');
+
+        if (Auth::attempt($credentials)){
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email'=>'Invalid credentials.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
